21ba007694e42e6460f37d29a199cbf650730ed0
Howle <mdv413@mail.ru>
2018-09-05 16:32:00 +0300

    Add keyEngin 1, 2

--- main.cpp
+++ main.cpp
@@ -12,6 +12,8 @@
 #include <vector>
 #include <ios>
 
+#include "math.h"
+
 #define file_in_len 100
 #define key_len 31
 
@@ -28,9 +30,49 @@ bool necessary_length(size_t &all, size_t &block){
         return false;
     }
 }
-void keyGen(char* &key){
+void keyMenu(char* &key){
     cout << "Введите ключ (от 10 до 30 символов):" << endl;
     cin >> key;
+    while (sizeof(key) < 10 || sizeof(key) > 30){
+        cout << "Ошибка генерации ключа!!!" << endl;
+        cout << "Введите ключ (от 10 до 30 символов):" << endl;
+        cin >> key;
+    }
+}
+int keyEngin(int num, int position, int keyLen){
+    return (keyLen*( div((abs(position-num)), keyLen).quot + 1))-num;
+}
+int keyEngin1(int num, int position, int keyLen){
+    return (position - div(num, keyLen).rem) > 0 ? keyLen-(position - div(num, keyLen).rem) : keyLen+(position - div(num, keyLen).rem) ;
+}
+int keyEngin2(int num, int position, int keyLen){
+    return (position + div(num, keyLen).rem) > 12 ? (position + div(num, keyLen).rem)-keyLen : (position + div(num, keyLen).rem) ;
+}
+void keyGen(char* &key){
+    char* ComandKey;
+    int tmpKey;
+    int currentPosition = 0;
+    keyMenu(key);
+    for (size_t i = 0; i < sizeof(key); i++) {
+        for (int t = 0; t < 2; t++) {
+            if (t == 0) {
+                tmpKey = int(*(key + i)) >> 4;
+                if ( tmpKey % 2 == 0){  //Если четный, то -, иначе +
+                        // -
+                        int num = tmpKey >> 1;
+                        if (currentPosition - num > 0) {
+                        currentPosition -= num;
+                    } else {
+                        
+                    }
+                }
+            } else {
+                
+            }
+        }
+    }
+    
+    
 }
 
 const char* pathin = "/Users/howle/prog/Cpp/projects/Coder/Coder/Coder/input.txt";
@@ -39,6 +81,8 @@ int main(int argc, const char * argv[]) {
     vector<char> str;
     /*char key[key_len];
     keyGen(key);*/
+    cout << keyEngin1(40, 2, 12) << endl;
+    cout << keyEngin2(-40, 2, 12) << endl;
     ifstream file(pathin, ios::binary);
     if ( file.is_open() ) {
         cout << "Файл открыт" << endl;
