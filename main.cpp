cdd6db1537df37a16711fbd33557626c0bb9af8b
Howle <mdv413@mail.ru>
2018-09-05 12:51:26 +0300

    Add key generator

--- main.cpp
+++ main.cpp
@@ -12,10 +12,15 @@
 #include <vector>
 #include <ios>
 
+#define file_in_len 100
+#define key_len 31
+
+using namespace std;
+
 bool necessary_length(size_t &all, size_t &block){
-    if (all >= 100) {
-        all-=100;
-        block = 100;
+    if (all >= file_in_len) {
+        all-=file_in_len;
+        block = file_in_len;
         return true;
     } else {
         block = all;
@@ -23,12 +28,17 @@ bool necessary_length(size_t &all, size_t &block){
         return false;
     }
 }
+void keyGen(char* &key){
+    cout << "Введите ключ (от 10 до 30 символов):" << endl;
+    cin >> key;
+}
 
 const char* pathin = "/Users/howle/prog/Cpp/projects/Coder/Coder/Coder/input.txt";
 
-using namespace std;
 int main(int argc, const char * argv[]) {
     vector<char> str;
+    /*char key[key_len];
+    keyGen(key);*/
     ifstream file(pathin, ios::binary);
     if ( file.is_open() ) {
         cout << "Файл открыт" << endl;
@@ -38,20 +48,22 @@ int main(int argc, const char * argv[]) {
     file.seekg(0, ios::end);
     size_t size_of_file = file.tellg();
     size_t counter;
-    file.seekg(0);
-    char der[10];
-    file >> der;
-    cout << der << endl;
+    size_t next_counter;
     file.seekg(0);
     bool state_counter = true;
     necessary_length(size_of_file, counter);
+    next_counter = counter;
     char * buf = new char[counter+1];
     while (state_counter) {
         file.read(buf, counter);
         cout << buf << endl;
         state_counter = necessary_length(size_of_file, counter);
     }
-    for ( int i = 0; i < str.size(); i++) cout << str[i];
+    //for ( int i = 0; i < str.size(); i++) cout << str[i];
+    cout << endl;
+    for ( size_t i = 0; i < next_counter; i++){
+        cout << *(buf+i) << endl;
+    }
     file.close();
     return 0;
 }
